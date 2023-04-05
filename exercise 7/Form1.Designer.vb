9c42ba2b3ea6636e1f7141373d9f198a973c04e7
JACQUES ROBINSON <JRobinso1172@slps.org>
2023-04-05 13:54:54 -0500

    New code man

--- exercise 7/Form1.Designer.vb
+++ exercise 7/Form1.Designer.vb
@@ -26,21 +26,21 @@ Partial Class Form1
         Me.btnCal = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.radGlazed = New System.Windows.Forms.RadioButton()
-        Me.radSugar = New System.Windows.Forms.RadioButton()
-        Me.radChoc = New System.Windows.Forms.RadioButton()
         Me.radFilled = New System.Windows.Forms.RadioButton()
-        Me.radNone = New System.Windows.Forms.RadioButton()
-        Me.radReg = New System.Windows.Forms.RadioButton()
+        Me.radChoc = New System.Windows.Forms.RadioButton()
+        Me.radSugar = New System.Windows.Forms.RadioButton()
+        Me.radGlazed = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.radCap = New System.Windows.Forms.RadioButton()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.lblSub = New System.Windows.Forms.Label()
+        Me.radReg = New System.Windows.Forms.RadioButton()
+        Me.radNone = New System.Windows.Forms.RadioButton()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.lblTotal = New System.Windows.Forms.Label()
         Me.lblSales = New System.Windows.Forms.Label()
+        Me.lblSub = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
@@ -52,7 +52,7 @@ Partial Class Form1
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(132, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(767, 39)
+        Me.Label1.Size = New System.Drawing.Size(742, 38)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Our doughnuts and coffee are the best in town!"
         '
@@ -90,6 +90,50 @@ Partial Class Form1
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Doughnut choices "
         '
+        'radFilled
+        '
+        Me.radFilled.AutoSize = True
+        Me.radFilled.Location = New System.Drawing.Point(24, 191)
+        Me.radFilled.Name = "radFilled"
+        Me.radFilled.Size = New System.Drawing.Size(127, 24)
+        Me.radFilled.TabIndex = 3
+        Me.radFilled.TabStop = True
+        Me.radFilled.Text = "&Filled ($1.50)"
+        Me.radFilled.UseVisualStyleBackColor = True
+        '
+        'radChoc
+        '
+        Me.radChoc.AutoSize = True
+        Me.radChoc.Location = New System.Drawing.Point(24, 137)
+        Me.radChoc.Name = "radChoc"
+        Me.radChoc.Size = New System.Drawing.Size(162, 24)
+        Me.radChoc.TabIndex = 2
+        Me.radChoc.TabStop = True
+        Me.radChoc.Text = "C&hocolate ($1.25)"
+        Me.radChoc.UseVisualStyleBackColor = True
+        '
+        'radSugar
+        '
+        Me.radSugar.AutoSize = True
+        Me.radSugar.Location = New System.Drawing.Point(24, 83)
+        Me.radSugar.Name = "radSugar"
+        Me.radSugar.Size = New System.Drawing.Size(126, 24)
+        Me.radSugar.TabIndex = 1
+        Me.radSugar.TabStop = True
+        Me.radSugar.Text = "&Sugar($1.05)"
+        Me.radSugar.UseVisualStyleBackColor = True
+        '
+        'radGlazed
+        '
+        Me.radGlazed.AutoSize = True
+        Me.radGlazed.Location = New System.Drawing.Point(24, 38)
+        Me.radGlazed.Name = "radGlazed"
+        Me.radGlazed.Size = New System.Drawing.Size(140, 24)
+        Me.radGlazed.TabIndex = 0
+        Me.radGlazed.TabStop = True
+        Me.radGlazed.Text = "&Glazed ($1.05)"
+        Me.radGlazed.UseVisualStyleBackColor = True
+        '
         'GroupBox2
         '
         Me.GroupBox2.Controls.Add(Me.radCap)
@@ -102,6 +146,39 @@ Partial Class Form1
         Me.GroupBox2.TabIndex = 4
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Coffee choices"
+        '
+        'radCap
+        '
+        Me.radCap.AutoSize = True
+        Me.radCap.Location = New System.Drawing.Point(24, 159)
+        Me.radCap.Name = "radCap"
+        Me.radCap.Size = New System.Drawing.Size(175, 24)
+        Me.radCap.TabIndex = 2
+        Me.radCap.TabStop = True
+        Me.radCap.Text = "Ca&ppuccino ($2.75)"
+        Me.radCap.UseVisualStyleBackColor = True
+        '
+        'radReg
+        '
+        Me.radReg.AutoSize = True
+        Me.radReg.Location = New System.Drawing.Point(24, 95)
+        Me.radReg.Name = "radReg"
+        Me.radReg.Size = New System.Drawing.Size(145, 24)
+        Me.radReg.TabIndex = 1
+        Me.radReg.TabStop = True
+        Me.radReg.Text = "&Regular ($1.50)"
+        Me.radReg.UseVisualStyleBackColor = True
+        '
+        'radNone
+        '
+        Me.radNone.AutoSize = True
+        Me.radNone.Location = New System.Drawing.Point(24, 41)
+        Me.radNone.Name = "radNone"
+        Me.radNone.Size = New System.Drawing.Size(69, 24)
+        Me.radNone.TabIndex = 0
+        Me.radNone.TabStop = True
+        Me.radNone.Text = "&None"
+        Me.radNone.UseVisualStyleBackColor = True
         '
         'GroupBox3
         '
@@ -118,118 +195,6 @@ Partial Class Form1
         Me.GroupBox3.TabIndex = 5
         Me.GroupBox3.TabStop = False
         '
-        'radGlazed
-        '
-        Me.radGlazed.AutoSize = True
-        Me.radGlazed.Location = New System.Drawing.Point(24, 38)
-        Me.radGlazed.Name = "radGlazed"
-        Me.radGlazed.Size = New System.Drawing.Size(140, 24)
-        Me.radGlazed.TabIndex = 0
-        Me.radGlazed.TabStop = True
-        Me.radGlazed.Text = "&Glazed ($1.05)"
-        Me.radGlazed.UseVisualStyleBackColor = True
-        '
-        'radSugar
-        '
-        Me.radSugar.AutoSize = True
-        Me.radSugar.Location = New System.Drawing.Point(24, 83)
-        Me.radSugar.Name = "radSugar"
-        Me.radSugar.Size = New System.Drawing.Size(126, 24)
-        Me.radSugar.TabIndex = 1
-        Me.radSugar.TabStop = True
-        Me.radSugar.Text = "&Sugar($1.05)"
-        Me.radSugar.UseVisualStyleBackColor = True
-        '
-        'radChoc
-        '
-        Me.radChoc.AutoSize = True
-        Me.radChoc.Location = New System.Drawing.Point(24, 137)
-        Me.radChoc.Name = "radChoc"
-        Me.radChoc.Size = New System.Drawing.Size(162, 24)
-        Me.radChoc.TabIndex = 2
-        Me.radChoc.TabStop = True
-        Me.radChoc.Text = "C&hocolate ($1.25)"
-        Me.radChoc.UseVisualStyleBackColor = True
-        '
-        'radFilled
-        '
-        Me.radFilled.AutoSize = True
-        Me.radFilled.Location = New System.Drawing.Point(24, 191)
-        Me.radFilled.Name = "radFilled"
-        Me.radFilled.Size = New System.Drawing.Size(127, 24)
-        Me.radFilled.TabIndex = 3
-        Me.radFilled.TabStop = True
-        Me.radFilled.Text = "&Filled ($1.50)"
-        Me.radFilled.UseVisualStyleBackColor = True
-        '
-        'radNone
-        '
-        Me.radNone.AutoSize = True
-        Me.radNone.Location = New System.Drawing.Point(24, 41)
-        Me.radNone.Name = "radNone"
-        Me.radNone.Size = New System.Drawing.Size(69, 24)
-        Me.radNone.TabIndex = 0
-        Me.radNone.TabStop = True
-        Me.radNone.Text = "&None"
-        Me.radNone.UseVisualStyleBackColor = True
-        '
-        'radReg
-        '
-        Me.radReg.AutoSize = True
-        Me.radReg.Location = New System.Drawing.Point(24, 95)
-        Me.radReg.Name = "radReg"
-        Me.radReg.Size = New System.Drawing.Size(145, 24)
-        Me.radReg.TabIndex = 1
-        Me.radReg.TabStop = True
-        Me.radReg.Text = "&Regular ($1.50)"
-        Me.radReg.UseVisualStyleBackColor = True
-        '
-        'radCap
-        '
-        Me.radCap.AutoSize = True
-        Me.radCap.Location = New System.Drawing.Point(24, 159)
-        Me.radCap.Name = "radCap"
-        Me.radCap.Size = New System.Drawing.Size(175, 24)
-        Me.radCap.TabIndex = 2
-        Me.radCap.TabStop = True
-        Me.radCap.Text = "Ca&ppuccino ($2.75)"
-        Me.radCap.UseVisualStyleBackColor = True
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(20, 30)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(75, 20)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "Subtotal:"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(20, 111)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(83, 20)
-        Me.Label3.TabIndex = 1
-        Me.Label3.Text = "Sales tax:"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(20, 183)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(83, 20)
-        Me.Label4.TabIndex = 2
-        Me.Label4.Text = "Total due:"
-        '
-        'lblSub
-        '
-        Me.lblSub.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblSub.Location = New System.Drawing.Point(101, 23)
-        Me.lblSub.Name = "lblSub"
-        Me.lblSub.Size = New System.Drawing.Size(82, 38)
-        Me.lblSub.TabIndex = 3
-        '
         'lblTotal
         '
         Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
@@ -245,6 +210,41 @@ Partial Class Form1
         Me.lblSales.Name = "lblSales"
         Me.lblSales.Size = New System.Drawing.Size(82, 38)
         Me.lblSales.TabIndex = 4
+        '
+        'lblSub
+        '
+        Me.lblSub.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSub.Location = New System.Drawing.Point(117, 23)
+        Me.lblSub.Name = "lblSub"
+        Me.lblSub.Size = New System.Drawing.Size(82, 38)
+        Me.lblSub.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 183)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 20)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Total due:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 20)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Sales tax:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 20)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Subtotal:"
         '
         'Form1
         '
